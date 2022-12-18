2389a5d9022fe9317fe1a4a6920356e8b94ae0f6
AniaKB <aniakbennett@gmail.com>
2022-12-18 11:26:22 -0500

    wrong password redirect php

--- answers.php/q1.php
+++ answers.php/q1.php
@@ -3,4 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$pass = $_POST['password'];
+$pass = $_POST['pw1'];
+
+if($pw1 != "bark")
+  {
+    header("Location: wrong.php");
+    exit();
+  }
+
+?>
---
+++ answers.php/wrong.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@200&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <img src="passwordcat.png" class="rotate">
+    <h1>That's not the password!!</h1>
+  </body>
