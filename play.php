131a11699e0f8c13006ee6dcd61d976f29750516
AniaKB <aniakbennett@gmail.com>
2022-12-18 10:56:25 -0500

    adding php pages

--- play.php
+++ play.php
@@ -1,6 +1,11 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=ZCOOL+XiaoWei&display=swap" rel="stylesheet">
