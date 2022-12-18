d64dd6292cd47a17f9f6e7783d5f117fca06172a
AniaKB <aniakbennett@gmail.com>
2022-12-18 15:58:55 -0500

    creating secret content, mustache/sunglasses buttons

---
+++ answers/q5.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$pass = $_POST['pw5'];
+
+if($pass !="sandy claws" || "Sandy claws");
+  {
+    header("Location: wrong.php");
+    exit();
+  }
+  else {
+    header("Location: content/bob.html");
+    exit();
+    };
+  ?>
