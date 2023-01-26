134c6fe921af7cf86b3706e6c144a49647a3cc40
AniaKB <aniakbennett@gmail.com>
2023-01-25 22:20:01 -0500

    finishing up q2

--- answers/q2.php
+++ answers/q2.php
@@ -3,3 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$pass = $_POST['pw2'];
+
+if($pass != "bark")
+  {
+    header("Location: wrong.php");
+    exit();
+  }
+  else {
+    header("Location: /owen-wilson.php");
+    exit();
+  };
+?>
